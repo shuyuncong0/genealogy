f2640c5874206fc6dd237286d067b84f87f021a5
succ0655 <succ0655@cairenhui.com>
2019-12-29 13:40:45 +0800

    123

--- src/store.js
+++ src/store.js
@@ -3,11 +3,11 @@ import Vuex from 'vuex'
 
 Vue.use(Vuex)
 const port = 8091
-// const host = '192.168.2.186' //测试 ss
+// const host = '192.168.2.186' //测试 sss
 
 // const host = 'localhost' //攀钢
 const host = '47.98.189.101'
-const apiUrl = `https://${host}:${port}/genealogy/`
+const apiUrl = `http://${host}:${port}/genealogy/`
 export default new Vuex.Store({
   state: {
     // 公共变量
